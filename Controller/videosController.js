1588a00e44754855646971f07d3484d00ccb44e8
Marleny Molina <marlenymolina39@gmail.com>
2024-06-24 18:02:44 -0600

    Actualizacion datos

--- Controller/videosController.js
+++ Controller/videosController.js
@@ -1,88 +1,106 @@
-const express = require('express');
-const Model = require('..//Models/videosModel');
-const router = express.Router()
-//Post Method
-router.post('/post', async (req, res) => {
+const Video = require('../Models/videosModel');
+
+/**
+ * Crea una registro
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const videoPost = async (req, res) => {
+    const { nombre, url, descripcion } = req.body;
     try {
-        // Verificar si ya existe un documento con el mismo 'code'
-        const existingCourse = await Model.findOne({ code: req.body.code});
-        const existingname = await Model.findOne({name: req.body.name});
-        if (existingCourse) {
-            return res.status(400).json({ message: 'El código del curso ya existe, favor ingrese uno nuevo' });
+        const newVideo = new Video({ nombre, url, descripcion });
+        const savedVideo = await newVideo.save();
+        res.status(201).json({ video: savedVideo, location: `/api/videos/${savedVideo._id}` });
+    } catch (error) {
+        console.error('Error al guardar el video:', error);
+        res.status(500).json({ error: 'Hubo un error al guardar el video. Por favor, intenta de nuevo más tarde.' });
+    }
+};
+
+/**
+ *Obtiene videos
+ * @param {*} req
+ * @param {*} res
+ */
+const videoGet = async (req, res) => {
+    try {
+        const videos = await Video.find();
+        res.json(videos);
+    } catch (error) {
+        console.error('Error al obtener videos:', error);
+        res.status(500).json({ error: 'Hubo un error al obtener los videos. Por favor, intenta de nuevo más tarde.' });
+    }
+};
+
+/**
+ * Elimina el video
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const videoDelete = async (req, res) => {
+    const { id } = req.params; // Obtener el ID desde la ruta
+    try {
+        const deletedVideo = await Video.findByIdAndDelete(id);
+        if (!deletedVideo) {
+            res.status(404).json({ error: 'El video que intentas eliminar no existe.' });
+        } else {
+            res.json({ message: 'El video ha sido eliminado correctamente' });
         }
-        else if(existingname){
-            return res.status(400).json({ message: 'El nombre del curso ya existe, favor ingrese uno nuevo' });
+    } catch (error) {
+        console.error('Error al eliminar el video:', error);
+        res.status(500).json({ error: 'Hubo un error al eliminar el video. Por favor, intenta de nuevo más tarde.' });
+    }
+};
+
+/**
+ * Actualiza un video 
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const videoUpdate = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { url, nombre, descripcion } = req.body;
+
+        // Verificar si el video existe en la base de datos
+        const video = await Video.findById(id);
+        if (!video) {
+            return res.status(404).json({ error: 'El video que intentas actualizar no existe.' });
         }
 
-        // Crear un nuevo documento
-        const data = new Model({
-            name: req.body.name,
-            code: req.body.code,
-            description: req.body.description,
-        });
+        // Actualizar la URL de YouTube
+        if (url !== undefined && url.trim() !== '') {
+            video.url = url.trim();
+        }
 
-        // Guardar el nuevo documento en la base de datos
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave);
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
+        // Actualizar el nombre del video
+        if (nombre) {
+            video.nombre = nombre;
+        }
 
-//Get all Method
-router.get('/getAll', async (req, res) => {
-    try{
-        const data = await Model.find();
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+        //Actualizar la descripcion del video
+        if (descripcion){
+            video.descripcion = descripcion;
+        }
 
+        // Guardar los cambios en el video actualizado
+        await video.save();
 
-//Update by ID Method
-//Update by ID Method
-router.patch('/update/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
+        // Responder con el video actualizado
+        return res.status(200).json({ message: 'Video actualizado correctamente' });
+    } catch (error) {
+        // Manejar errores
+        console.error('Error al actualizar el video:', error);
+        return res.status(500).json({ error: 'Hubo un error al actualizar el video. Por favor, intenta de nuevo más tarde.' });
+    }
+};
 
-        const result = await Model.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        res.send(result)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
-
-//Delete by ID Method
-//Delete by ID Method
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Model.findByIdAndDelete(id)
-        res.send(`Document with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
-
-//Get by ID Method
-//Get by ID Method
-router.get('/getOne/:id', async (req, res) => {
-    try{
-        const data = await Model.findById(req.params.id);
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
-module.exports = router;
+module.exports = {
+    videoPost,
+    videoGet,
+    videoDelete,
+    videoUpdate
+};
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # KidsTube_Backend_V1
+
+## Descripción
+
+KidsTube_Backend_V1 es una aplicación web destinada a padres que deseen controlar los contenidos que sus hijos pueden ver en línea. Permite a los padres seleccionar los contenidos específicos a los que sus hijos tienen acceso. Estos contenidos pueden ser videos de YouTube o videos que los padres carguen directamente a la lista de videos.
+
+La aplicación utiliza Node.js como entorno de ejecución, Express.js como framework web y Mongoose como biblioteca de modelado de objetos para MongoDB, lo que proporciona una base sólida para la construcción de una API RESTful para gestionar los datos de los usuarios y los videos.
+
+## Tecnologías utilizadas
+
+- **Node.js:** Plataforma de tiempo de ejecución de JavaScript que permite la construcción de aplicaciones del lado del servidor.
+
+- **Express.js:** Framework web de Node.js que facilita la creación de aplicaciones web y APIs.
+
+- **Mongoose:** Biblioteca de modelado de objetos para MongoDB, que proporciona una interfaz sencilla para trabajar con bases de datos MongoDB desde Node.js.
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local utilizando `git clone`.
+2. Navega hasta el directorio del proyecto.
+3. Instala las dependencias utilizando `npm install`.
+4. Configura las variables de entorno necesarias, como la cadena de conexión a la base de datos MongoDB.
+5. Inicia la aplicación utilizando `npm start`.
