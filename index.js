5fe7900cf6f3bad26c456846b81a3c4fbbf5e0b0
Karen-2000 <karen2000pje@gmail.com>
2024-07-03 23:01:52 -0600

    edition users

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
-const db = mongoose.connect("mongodb://0.0.0.0:27017/tubekids"); 
+const mongoString = process.env.DATABASE_URL////KAREN CONECTA BD
+const db = mongoose.connect(mongoString); ////KAREN CONECTA BD
 
 const database = mongoose.connection;
 
