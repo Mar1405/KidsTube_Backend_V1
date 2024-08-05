51a0dbb6c74d8f644b0c2c83b58e2a073e459815
Marleny Molina <marlenymolina39@gmail.com>
2024-08-05 08:07:27 -0600

    Actualizacion

--- Controller/avatarUserkidsController.js.js
+++ Controller/avatarUserkidsController.js.js
@@ -5,7 +5,7 @@ const AvatarPost = async (req, res) => {
 
     try {
         // Busca el usuario por el nombre del avatar y la contraseña proporcionada
-        const user = await Users.findOne({ name: avatarName, password });
+        const user = await Users.findOne({name: avatarName, password });
         if (!user) {
             return res.status(404).json({ error: 'Nombre de avatar o contraseña incorrectos' });
         }
@@ -13,11 +13,13 @@ const AvatarPost = async (req, res) => {
         // Si se encuentra el usuario con el nombre de avatar y la contraseña coincidente
         return res.status(200).json({ message: 'Contraseña correcta!' });
         
+
     } catch (error) {
         console.error('Error al intentar acceder como avatar', error);
         return res.status(500).json({ error: 'Hubo un error al intentar acceder como avatar' });
     }
 };
+
 module.exports = {
     AvatarPost
 };
